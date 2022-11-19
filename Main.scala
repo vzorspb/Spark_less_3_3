a7e16085d91c54cc27619c2a7cd6b59243bb9a47
Andrei Nevzorov <vzor.mobile@gmail.com>
2022-11-19 21:48:52 +0300

    modified:   Main.scala

--- Main.scala
+++ Main.scala
@@ -48,13 +48,13 @@ object Main extends App {
     )
     var dfAction = spark.createDataFrame (spark.sparkContext.parallelize (actionsData), actionsSchema)
     var dfTags = spark.createDataFrame (spark.sparkContext.parallelize (tagsData), tagsSchema)
-    for (a <- 1 to datsFrameSize) {
-      pagesTagsData = pagesTagsData ++ Seq (Row (userData.lift (Random.nextInt (userData.length) ).get (0), new sql.Timestamp ("1668799353153".toLong - Random.nextInt ().abs.toLong * 1000), actionsData.lift (Random.nextInt (actionsData.length) ).get (1), Random.nextInt (101), tagsData.lift (Random.nextInt (tagsData.length) ).get (1), Random.nextInt (2) == 1) )
+    for (a <- 1 to datsFrameSize-1) {
+      pagesTagsData = pagesTagsData ++ Seq (Row (userData.lift (Random.nextInt (userData.length) ).get (0), new sql.Timestamp ("1668799353153".toLong - Random.nextInt ().abs.toLong * 1000), actionsData.lift (Random.nextInt (actionsData.length) ).get (1), Random.nextInt (10), tagsData.lift (Random.nextInt (tagsData.length) ).get (1), Random.nextInt (2) == 1) )
     }
     return spark.createDataFrame (spark.sparkContext.parallelize (pagesTagsData), pagesTagsSchema)
   }
 
-
+    val lines = 200 //количество генерируемых строк датафрейма
     val spark = SparkSession.builder
       .master("spark://192.168.251.107:7077")
       .appName("Spark Task 3.3")
@@ -62,12 +62,12 @@ object Main extends App {
       .config("spark.driver.extraClassPath","C:/Users/nevzorov.KB/IdeaProjects/Spark_3_3/out/artifacts/Spark_3_3_jar/Spark_3_3.jar")
       .getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
-    var dfPages = generateDataFrame (100)
+    var dfPages = generateDataFrame (200)
     dfPages.show()
   //  dfPages.coalesce(1).write.mode("overwrite").option("header","true").csv("hdfs://192.168.251.105/df/df.csv")
   println("Топ 5 активных пользователей")
    dfPages.groupBy("userId").count().orderBy(col("count").desc).show(5)
-
-
+   println("Процент посетителей, у которых есть ЛК")
+  dfPages.groupBy("sign").count().withColumn("procents",col("count")/lines*100).select("procents").show(1)
   //продолжение следует ...
 }
